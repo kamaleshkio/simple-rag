8949eefa7c25c68ade350daf6eff3a0830b531a0
Kamalesh <138756510+kamaleshkio@users.noreply.github.com>
2025-06-27 13:58:29 +0530

    Update llm_chain.py

--- components/llm_chain.py
+++ components/llm_chain.py
@@ -1,9 +1,9 @@
-from langchain_openai import ChatopenAI
+from langchain_openai import ChatOpenAI
 from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 def get_llm_chain(api_key, model_name = "mistralai/mistral-7b-instruct"):
-    llm = ChatopenAI(
+    llm = ChatOpenAI(
         openai_api_key = api_key,
         openai_api_base = "https://openrouter.ai/api/v1",
         model_name = model_name,
