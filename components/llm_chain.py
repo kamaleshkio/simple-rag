325a7a14103487e3e45c7a65e933a9aae1440d44
kamalesh <kamaleshbaskaran4310@gmail.com>
2025-06-27 14:19:42 +0530

    llm chain update

--- components/llm_chain.py
+++ components/llm_chain.py
@@ -2,16 +2,17 @@ from langchain_openai import ChatOpenAI
 from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
-def get_llm_chain(api_key, model_name = "mistralai/mistral-7b-instruct"):
+def get_llm_chain(api_key, model = "mistralai/mistral-7b-instruct"):
     llm = ChatOpenAI(
+        open_api_base = "https://openrouter.ai/api/v1",
         openai_api_key = api_key,
         openai_api_base = "https://openrouter.ai/api/v1",
-        model_name = model_name,
+        model = model,
         temperature = 0.8,
     )
 
 
-    prompt = ChatPromptTemplate("""
+    prompt = ChatPromptTemplate.from_template("""
     use the following context to answer the question.
     <context>
     {context}                            
