69a8495c2decf468f8267f5b62ed2d304e2145ad
kamalesh <kamaleshbaskaran4310@gmail.com>
2025-06-28 15:06:20 +0530

    loader update

--- components/load_pdf.py
+++ components/load_pdf.py
@@ -1,5 +1,6 @@
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader
 
 def load_pdf(path):
-    loader = PyPDFLoader("Bakthi.pdf")
+    #loader = PyPDFLoader("Bakthi.pdf")
+    loader = WebBaseLoader("https://aws.amazon.com/what-is/retrieval-augmented-generation/")
     return loader.load()
